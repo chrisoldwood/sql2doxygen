f5f47fae1c8b16ae1ece81660c91212b0a85daf1
Admin <admin@chrisoldwood.com>
2013-01-12 09:27:31 +0000

    Inital version.

---
+++ Example/Schemas.sql
@@ -0,0 +1,13 @@
+/**
+** \file
+** \brief  Schema documentation.
+** \author Chris Oldwood
+**/
+
+/**
+** \namespace dbo
+** The default schema for database objects.
+**
+** If no schema name is specified as part of an object or type's name then this
+** is assumed to be schema it's defined within.
+**/
